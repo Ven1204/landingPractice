0a3a02f071046e7841ffad2cb4e1fb86b9b13759
Rayven <rayven.rey@gmail.com>
2022-06-22 14:37:36 +0200

    added dependency threeJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { init } from './utils/initCarLayer';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    init()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
