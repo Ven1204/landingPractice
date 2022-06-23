f8796e53dc7fa22a0a58a83df3d33830de857357
Rayven <rayven.rey@gmail.com>
2022-06-23 16:01:27 +0200

    animated model

--- src/utils/initCarLayer.js
+++ src/utils/initCarLayer.js
@@ -9,7 +9,7 @@ const loader = new GLTFLoader();
 export function init() {
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 2;
+	camera.position.z = 1.5;
     camera.rotation.z = 0;
     camera.position.y = .2;
 
@@ -25,8 +25,8 @@ export function init() {
 
         gsap.to(camera.position, {
             z: 1,
-            duration: 2,
-            ease: "back.out(3)",
+            duration: 3,
+            ease: "back.out(1)",
             y: .2
         })
         // gsap.to(camera.rotation, {
@@ -34,31 +34,39 @@ export function init() {
         //     duration: 1
         // })
 
-        // gsap.to(model.rotation, {
-        //     x: 1,
-        //     duration: 1,
-        //     delay: 1
-        // })
-        // gsap.to(model.rotation, {
-        //     y: Math.PI * 1.75,
-        //     duration: 2,
-        //     delay: 1
-        // })
-        // gsap.to(model.scale, {
-        //     delay: 1,
-        //     duration: 1,
-        //     x: .25,
-        //     y: .25,
-        //     z: .25
-        // })
-        // gsap.to(model.position, {
-        //     delay: 1,
-        //     duration: 1,
-        //     x: .35,
-        //     y: .3
-        // })
+        gsap.to(model.rotation, {
+            x: 0.2,
+            y: -0.4,
+            duration: 1,
+            delay: 1
+        })
+        gsap.to(model.rotation, {
+            y: Math.PI * 1.7,
+            x: .3,
+            duration: 2,
+            delay: 2
+        })
 
+        gsap.to(model.position, {
+            delay: 4,
+            duration: 1,
+            x: .4,
+            y: .40
+        })
 
+        gsap.to(model.rotation, {
+          delay: 4,
+          duration: 1,
+          x: .5,
+        })
+
+        gsap.to(model.scale, {
+          delay: 4,
+          duration: 1,
+          x: .15,
+          y: .15,
+          z: -.15
+        })
 
         scene.add(model)
     })
@@ -66,7 +74,7 @@ export function init() {
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setAnimationLoop( animation );
-    renderer.setClearColor( 0x24242424, 1 );
+    renderer.setClearColor( 0x282828, 1 );
 	document.body.appendChild( renderer.domElement );
 
     window.addEventListener( 'resize', () => {
