33a1ff3f13fabab9b465bce83c6722f77fd1933d
Rayven <rayven.rey@gmail.com>
2022-06-23 16:16:14 +0200

    added jsx

--- src/App.js
+++ src/App.js
@@ -8,7 +8,38 @@ function App() {
 
   return (
     <div className="App">
+      <div className="hero">
+        <h1 className="title">Cars</h1>
+        <p className="text">asdgfbsjdfnsajddaflknafgoa´gsgfoáhigdsljdgfahg´WOJF´JGIOÑHGAOIGAGSKÑNGFDJ´LSFJLCKVBLNdslfgbsfds</p>
+        <p>
+          <button className="btn">English</button>
+          <button className="btn">Spanish</button>
 
+        </p>
+      </div>
+
+      <div className="pricing">
+        <h1>Our Packages</h1>
+        <div className="pricing-table">
+          <div className="pricing-card">
+            <p className="title">€ 999999</p>
+            <p className="info">asdfgsdfsf</p>
+            <button className="btn">Buy now</button>
+          </div>
+
+          <div className="pricing-card">
+            <p className="title">€ 999999</p>
+            <p className="info">asdfgsdfsf</p>
+            <button className="btn">Buy now</button>
+          </div>
+
+          <div className="pricing-card">
+            <p className="title">€ 999999</p>
+            <p className="info">asdfgsdfsf</p>
+            <button className="btn">Buy now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
